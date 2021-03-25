1516e1929241c0c81e5c3b4e493d103fe7194d02
khoeriyah <khoeriyah216@gmail.com>
2021-03-25 12:19:27 +0700

    FAQ

---
+++ FAQ/Training.php
@@ -0,0 +1,12 @@
+<body>
+<table border=3 class="center">
+<tr>
+<td>
+<h3 style="color:rgba(91, 203, 173, 1);"><strong>PERTANYAAN 9</strong></h3>    
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ea quibusdam ut. Earum saepe eius cumque optio tenetur quod dolorem, numquam nihil iusto fugit ratione, perferendis eaque esse possimus quia!</p>
+<td>
+<h3 style="color:rgba(91, 203, 173, 1);">PERTANYAAN 10</h3>    
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ea quibusdam ut. Earum saepe eius cumque optio tenetur quod dolorem, numquam nihil iusto fugit ratione, perferendis eaque esse possimus quia!</p>
+
+</table>
+</body>
